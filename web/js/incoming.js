55648901574f7cc5185f63b813c07a84c3a73d54
snakie <snakie@yahoo.com>
2014-01-31 01:01:45 -0800

    clean up fe
    Former-commit-id: fdad012e552efec657061d35f7ec6db4df969c98

--- web/js/incoming.js
+++ web/js/incoming.js
@@ -2,27 +2,27 @@ var last_block = 0;
 
 function block_to_row(block) {
        var cells = [];
-       cells.push("<tr>");
-       cells.push("<td><a href=\"/api/blocks/"+block.id+"\">"+block.id+"</td>");
-       cells.push("<td><abbr class=\"timeago\" title=\""+block.time+"\">"+block.time+"</abbr></td>");
-       cells.push("<td>"+(block.pos == true ? "POS" : "POW")+"</td>");
-       cells.push("<td>"+(block.pos == true ? parseFloat(block.diff).toFixed(2) : (parseFloat(block.diff) / 1e6).toFixed(2) + 'M')+"</td>");
-       cells.push("<td>"+parseFloat(block.reward).toFixed(2)+"</td>");
-       cells.push("<td>"+block.txcount+"</td>");
-       cells.push("<td>"+parseFloat(block.received).toFixed(2)+"</td>");
-       cells.push("<td>"+parseFloat(block.destroyed).toFixed(2)+"</td>");
-       cells.push("<td>"+(block.pos == true ? parseFloat(block.staked) : '-')+"</td>");
-       cells.push("<td>"+(block.pos == true ? block.coindays : '-')+"</td>");
-       cells.push("</tr>");
+       cells.push("<div class=\"row\">");
+       cells.push("<div class=\"col-md-1\"><a href=\"/api/blocks/"+block.id+"\">"+block.id+"</a></div>");
+       cells.push("<div class=\"col-md-2\"><abbr class=\"timeago\" title=\""+block.time+"\">"+block.time+"</abbr></div>");
+       cells.push("<div class=\"col-md-1\">"+(block.pos == true ? "POS" : "POW")+"</div>");
+       cells.push("<div class=\"col-md-1\">"+(block.pos == true ? parseFloat(block.diff).toFixed(2) : (parseFloat(block.diff) / 1e6).toFixed(2) + 'M')+"</div>");
+       cells.push("<div class=\"col-md-1\">"+parseFloat(block.reward).toFixed(2)+"</div>");
+       cells.push("<div class=\"col-md-1\">"+block.txcount+"</div>");
+       cells.push("<div class=\"col-md-1\">"+parseFloat(block.received).toFixed(2)+"</div>");
+       cells.push("<div class=\"col-md-1\">"+parseFloat(block.destroyed).toFixed(2)+"</div>");
+       cells.push("<div class=\"col-md-1\">"+(block.pos == true ? parseFloat(block.staked) : '-')+"</div>");
+       cells.push("<div class=\"col-md-1\">"+(block.pos == true ? block.coindays : '-')+"</div>");
+       cells.push("</div>");
        return cells;
 }
 function txn_to_row(tx) {
     var cells = []
-    cells.push("<tr>");
-    cells.push("<td>"+tx.hash+"</td>");
-    cells.push("<td><abbr class=\"timeago\" title=\""+tx.time+"\">"+tx.time+"</abbr></td>");
-    cells.push("<td>"+parseFloat(tx.value).toFixed(2)+"</td>");
-    cells.push("</tr>");
+    cells.push("<div class=\"row\">");
+    cells.push("<div class=\"col-md-6\">"+tx.hash+"</div>");
+    cells.push("<div class=\"col-md-2\"><abbr class=\"timeago\" title=\""+tx.time+"\">"+tx.time+"</abbr></div>");
+    cells.push("<div class=\"col-md-1\">"+parseFloat(tx.value).toFixed(2)+"</div>");
+    cells.push("</div>");
     return cells;
 }
 function add_block(block) {
@@ -30,18 +30,18 @@ function add_block(block) {
     update_last_block(block);
     var cells = block_to_row(block);
     $("#table_header").after(cells.join(""));
-    console.log("block length: "+$("#blocks tr").size());
-    if($("#blocks tr ").size() > 11) {
-        $("#blocks tr:last").remove();
+    console.log("block length: "+$("#blocks div.row").size());
+    if($("#blocks div.row").size() > 11) {
+        $("#blocks div.row:last").remove();
     }
     $("abbr.timeago").timeago();
 }
 function add_tx(tx) {
     var cells = txn_to_row(tx)
     $("#txn_header").after(cells.join(""));
-    console.log("txn length: "+$("#txns tr").size());
-    if($("#txns tr ").size() > 11) {
-        $("#txns tr:last").remove();
+    console.log("txn length: "+$("#txns.row div").size());
+    if($("#txns div.row").size() > 11) {
+        $("#txns div.row:last").remove();
     }
     $("abbr.timeago").timeago();
 
