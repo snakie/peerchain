012e87fb5d6c8d0ca856334c8f5899be846aae16
snakie <snakie@yahoo.com>
2014-03-16 18:18:33 -0700

    preload cache after new block comes in
    Former-commit-id: 30a9ec428716e56761cb9837a67daa052fd60c35

--- bin/block.sh
+++ bin/block.sh
@@ -5,6 +5,10 @@ hash=$1
 echo "`date` block $hash" >> /app/logs/new.log
 if [ ! -f /app/conf/killswitch ]; then
     /usr/local/bin/python /app/bin/sync_server.py -b $hash &>> /app/logs/sync_server.log 
+    BLOCKS=$(curl -s http://localhost/api/blocks/count)
+    curl -s -o /dev/null http://localhost/api/series/diff/$BLOCKS &
+    curl -s -o /dev/null http://localhost/api/series/money_supply/$BLOCKS &
+    curl -s -o /dev/null http://localhost/api/series/inflation_rate/$BLOCKS &
 fi
 
 
