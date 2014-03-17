1e40c398902c0142ae9281aa034b707104e0bf4d
snakie <snakie@yahoo.com>
2014-03-16 19:38:21 -0700

    link from inflation rate to charts.html
    Former-commit-id: d9e20a3fe6debf318a05bb2aababa122a5387f37

--- web/js/incoming.js
+++ web/js/incoming.js
@@ -49,7 +49,7 @@ function compare_to_row(compare) {
     cells.push("<td><a href=\"/api/compare/delta/"+compare.last_block+"/"+network_review+"\">"+compare.last_block+"</td>");
     cells.push("<td><a href=\"/api/network/"+compare.last_block+"\">"+parseFloat(compare.money_supply_end).toFixed(2)+"</td>");
     cells.push("<td>"+parseFloat(100*compare.pos_blocks/(compare.pos_blocks+compare.pow_blocks)).toFixed(2)+"%</td>");
-    cells.push("<td>"+compare.inflation_rate+"%</td>");
+    cells.push("<td><a href=\"charts.html\">"+compare.inflation_rate+"%</a></td>");
     cells.push("<td>"+parseFloat(compare.mined_coins).toFixed(2)+"</td>");
     cells.push("<td>"+parseFloat(compare.minted_coins).toFixed(2)+"</td>");
     cells.push("<td>"+parseFloat(compare.destroyed_fees).toFixed(2)+"</td>");
